6b7974fd4d1cab54919b9d2cda26a36f72b35cac
Nikita <Nikita@ASUS>
2024-07-30 08:43:47 +0530

    Changes done !

--- Git/src/Old.java
+++ Git/src/Old.java
@@ -2,6 +2,6 @@
 public class Old {
 public static void main(String[] args) {
 	System.out.println("HIIIIIIIIIIIIIIIIIIIIII OLD");
-	System.out.println("HIIIIIIIIIIIIIIIIIIIIII OLD");
+    System.out.println("ITS NIKKK");
 }
 }
